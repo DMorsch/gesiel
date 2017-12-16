26cdac90d57dd7f82b76198f96fdd88b509e0500
Daniel Morsch <danielmorsch@hotmail.com>
2017-12-15 22:22:50 -0200

    mudei o componente Pessoa para um stateful component

--- src/componentes/Persons/Person/Person.js
+++ src/componentes/Persons/Person/Person.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
+class Person extends Component{
+    render() {
+        return <div className={classes.Persona} >
+        <p onClick={this.props.clique}> {this.props.nome} tem {this.props.idade } anos de idade </p>
+        <p> {this.props.children} </p>
+        <input type="text" onChange={this.props.mudar}/>
+    </div>
+    }
+}
+/*
 const person = (props) => {
 return (
 <div className={classes.Persona} >
@@ -9,6 +19,6 @@ return (
     <input type="text" onChange={props.mudar}/>
 </div>
 );
-}
+}*/
 
-export default person;
+export default Person;
